fa55271a831d928fb65b2ecf5d15bb1e29ab35c0
zGiusee <giusebelforte@gmail.com>
2024-01-31 16:30:46 +0100

    [UPDATE] base exercise 100% complete

--- db.php
+++ db.php
@@ -13,13 +13,19 @@ $pesce = new Category('pesce');
 $uccello = new Category('uccello');
 
 // Popolo l'array con i prodotti usando le classi
-$Products = [
+$productsFood = [
     new ItemFood('Royal Canin', 'Mini Adult', 4.34, 'https://arcaplanet.vtexassets.com/arquivos/ids/284621/Mini-Adult.jpg?v=638182891693570000', $cane, '1230', '1,5kg', '11/06/24'),
-    new ItemFood('Almo Nature', 'Holistic Maintenance Medium Large Tonno e Riso', 6, 'ttps://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg', $cane, '1300', '2kg', '23/07/26'),
-    new ItemFood('Almo Nature', 'Cat Daily Lattina', 25, 'https://arcaplanet.vtexassets.com/arquivos/ids/284621/Mini-Adult.jpg?v=638182891693570000', $gatto, '990', '350gr', '02/03/2030'),
+    new ItemFood('Almo Nature', 'Holistic Maintenance Medium Large Tonno e Riso', 6, 'https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg', $cane, '1300', '2kg', '23/07/26'),
+    new ItemFood('Almo Nature', 'Cat Daily Lattina', 25, 'https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg', $gatto, '990', '350gr', '02/03/2030'),
     new ItemFood('Tetra', 'Mangime per Pesci Guppy in Fiocchi', 2.56, 'https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg', $pesce, '322', '200gr', '10/03/2022'),
+];
+
+$productsAccessory = [
     new ItemAccessory('Voliera', 'Wilma in Legno', 78, 'https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg', $uccello, '5gk', 'Wood', 'High'),
     new ItemAccessory('EasyCrystal', 'Cartucce Filtranti per Filtro ', 5.33, 'https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg', $pesce, '100gr', 'Carbone Attivo', 'Medium'),
+];
+
+$productsGame = [
     new ItemGame('Kong', 'Kong Classic', 4.99, 'https://arcaplanet.vtexassets.com/arquivos/ids/256599/kong-classic1.jpg', $cane, '97gr', 'Plastic', 'Tiny'),
     new ItemGame('Trixie', 'Topini di peluche', 2.55, 'https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg', $gatto, '22gr', 'Peluche', 'Tiny'),
 ];
--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 require_once __DIR__ . '/db.php';
 
-var_dump($Products);
 
 
 ?>
@@ -19,12 +18,70 @@ var_dump($Products);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
-<body>
+<body class="bg-primary">
     <main>
         <div class="container">
-            <div class="row">
-                <div class="col-4"></div>
+
+            <!-- Sezione cibi -->
+            <div class="row py-5">
+                <?php foreach ($productsFood as $item) { ?>
+                    <div class="col-3">
+                        <div class="card" style="width: 18rem;">
+                            <img class="card-img-top" src="<?php echo $item->img ?>" alt="Card image cap">
+                            <div class="card-body text-center">
+                                <h5 class="card-title"><?php echo $item->itemName ?></h5>
+                                <h5 class="card-subtitle"><?php echo $item->manufacturingCompany ?></h5>
+                                <p class="card-text "><?php echo "Cibo per " . $item->category->name ?></p>
+                                <p class="card-text"><?php echo "Peso: " . $item->peso ?></p>
+                                <p class="card-text"><?php echo " Calorie: " . $item->kcals . " Data di scadenza " . $item->expire_date ?></p>
+                                <a href="#" class="btn btn-primary">Aggiungi al carrello</a>
+                                <p class="card-text">Prezzo:<?php echo $item->prezzo . "$" ?></p>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
             </div>
+
+            <!-- Sezione Accessori -->
+            <div class="row py-5">
+                <?php foreach ($productsAccessory as $item) { ?>
+                    <div class="col-3">
+                        <div class="card" style="width: 18rem;">
+                            <img class="card-img-top" src="<?php echo $item->img ?>" alt="Card image cap">
+                            <div class="card-body text-center">
+                                <h5 class="card-title"><?php echo $item->itemName ?></h5>
+                                <h5 class="card-subtitle"><?php echo $item->manufacturingCompany ?></h5>
+                                <p class="card-text "><?php echo "Cibo per " . $item->category->name ?></p>
+                                <p class="card-text"><?php echo "Peso: " . $item->peso ?></p>
+                                <p class="card-text"><?php echo " Materiale: " . $item->material . " Durabilità " . $item->durability ?></p>
+                                <a href="#" class="btn btn-primary">Aggiungi al carrello</a>
+                                <p class="card-text">Prezzo:<?php echo $item->prezzo . "$" ?></p>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+
+            <!-- Sezione Accessori -->
+            <div class="row py-5">
+                <?php foreach ($productsGame as $item) { ?>
+                    <div class="col-3">
+                        <div class="card" style="width: 18rem;">
+                            <img class="card-img-top" src="<?php echo $item->img ?>" alt="Card image cap">
+                            <div class="card-body text-center">
+                                <h5 class="card-title"><?php echo $item->itemName ?></h5>
+                                <h5 class="card-subtitle"><?php echo $item->manufacturingCompany ?></h5>
+                                <p class="card-text "><?php echo "Cibo per " . $item->category->name ?></p>
+                                <p class="card-text"><?php echo "Peso: " . $item->peso ?></p>
+                                <p class="card-text"><?php echo " Materiale: " . $item->material . " Grandezza: " . $item->size ?></p>
+                                <a href="#" class="btn btn-primary">Aggiungi al carrello</a>
+                                <p class="card-text">Prezzo:<?php echo $item->prezzo . "$" ?></p>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+
         </div>
     </main>
 </body>
