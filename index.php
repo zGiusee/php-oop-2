d1790e604cc26a6ea46a8cacd0977a7045540bb2
zGiusee <giusebelforte@gmail.com>
2024-01-31 14:41:49 +0100

    [UPDATE] Creato classi e sotto classi con incapsulazione classi + array contenitore

--- index.php
+++ index.php
@@ -1,4 +1,69 @@
 <?php
+
+// Definisco la classe padre ovvero 'Prodotti'
+class Item
+{
+    public $manufacturingCompany;
+    public $itemName;
+    public $prezzo;
+    public $img;
+
+
+    public function __construct($_manufacturingCompany, $_itemName, $_prezzo, $_img)
+    {
+        $this->manufacturingCompany = $_manufacturingCompany;
+        $this->itemName = $_itemName;
+        $this->prezzo = $_prezzo;
+        $this->img = $_img;
+    }
+}
+
+// Definisco la classe figlio di prodotti ovvero 'ItemSpecific'
+class ItemSpecific extends Item
+{
+    public $type;
+    public $animal;
+
+    function __construct($_manufacturingCompany, $_itemName, $_prezzo, $_img, $_type, Animals $_animal)
+    {
+        parent::__construct($_manufacturingCompany, $_itemName, $_prezzo, $_img);
+        $this->type = $_type;
+        $this->animal = $_animal;
+    }
+}
+
+// Classe animali
+class Animals
+{
+    public $name;
+
+    function __construct($_name)
+    {
+        $this->name = $_name;
+    }
+}
+
+// Aggiungo gli animali
+$cane = new Animals('cane');
+$gatto = new Animals('gatto');
+$pesce = new Animals('pesce');
+$uccello = new Animals('uccello');
+
+$Products = [
+    new ItemSpecific('Royal Canin', 'Mini Adult', 4.34, 'https://arcaplanet.vtexassets.com/arquivos/ids/284621/Mini-Adult.jpg?v=638182891693570000', 'cibo', $cane),
+    new ItemSpecific('Almo Nature', 'Holistic Maintenance Medium Large Tonno e Riso', 6, 'ttps://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg', 'cibo', $cane),
+    new ItemSpecific('Almo Nature', 'Cat Daily Lattina', 25, 'https://arcaplanet.vtexassets.com/arquivos/ids/284621/Mini-Adult.jpg?v=638182891693570000', 'cibo', $cane),
+    new ItemSpecific('Tetra', 'Mangime per Pesci Guppy in Fiocchi', 2.56, 'https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg', 'cibo', $pesce),
+    new ItemSpecific('Voliera', 'Wilma in Legno', 78, 'https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg', 'gabbia', $uccello),
+    new ItemSpecific('EasyCrystal', 'Cartucce Filtranti per Filtro ', 5.33, 'https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg', 'acquario', $pesce),
+    new ItemSpecific('Kong', 'Kong Classic', 4.99, 'https://arcaplanet.vtexassets.com/arquivos/ids/256599/kong-classic1.jpg', 'gioco', $cane),
+    new ItemSpecific('Trixie', 'Topini di peluche', 2.55, 'https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg', 'gioco', $gatto),
+];
+
+
+var_dump($Products);
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
--- readme.md
+++ readme.md
@@ -4,7 +4,8 @@ oggi pomeriggio provate ad immaginare quali sono le classi necessarie per creare
 - L'e-commerce vende prodotti per gli animali<br>
 - I prodotti saranno oltre al cibo, anche giochi e accessori (cucce, guinzagli, lettiere, acquari, etc)<br>
 - I prodotti possono appartenere a una tra queste categorie:
-Cane, Gatto, Uccelli, PesciUna volta create le classi che descrivono il nostro sistema, instanziate prima tutte le categorie che ci serviranno, poi create alcuni prodotti appartenenti a differenti categorie  e stampate delle card contenenti i dettagli dei prodotti, come immagine, titolo, prezzo, icona della categoria ed il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, accessorio)<br>
+Cane, Gatto, Uccelli, Pesci<br>
+Una volta create le classi che descrivono il nostro sistema, instanziate prima tutte le categorie che ci serviranno, poi create alcuni prodotti appartenenti a differenti categorie  e stampate delle card contenenti i dettagli dei prodotti, come immagine, titolo, prezzo, icona della categoria ed il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, accessorio)<br>
 ### BONUS:<br>
 Definite una proprietà privata ed un metodo privato all'interno di una classe, assegnategli un valore e mostratelo nelle card (ricordatevi dell'esempio del codice prodotto visto a lezione)<br>
 
